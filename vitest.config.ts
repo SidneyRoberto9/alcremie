5cec21a1da7de6b27f9f47239d35cd8560949fef
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-09-10 16:36:10 -0300

    🧪 test: setup vitest and zod

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import swc from 'unplugin-swc';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: {
+        type: 'es6',
+      },
+    }),
+  ],
+});
