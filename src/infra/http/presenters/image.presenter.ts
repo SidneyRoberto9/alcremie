93dee3555dc6b9c4b49fdcd9f9faf9368d75b1c8
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-09-13 16:45:43 -0300

    ✨ feat: create image presenter for upload image return

---
+++ src/infra/http/presenters/image.presenter.ts
@@ -0,0 +1,20 @@
+import { Image } from '@/domain/alcremie/enterprise/entities/image';
+
+export class ImagePresenter {
+  static toHTTP(image: Image) {
+    return {
+      id: image.id.toValue(),
+      assetId: image.assetId,
+      isNsfw: image.isNsfw,
+      size: image.size,
+      url: image.url,
+      createdAt: image.createdAt,
+      updatedAt: image.updatedAt,
+      tags: image.tags.map((tag) => ({
+        id: tag.id.toValue(),
+        name: tag.name,
+        slug: tag.slug.value,
+      })),
+    };
+  }
+}
