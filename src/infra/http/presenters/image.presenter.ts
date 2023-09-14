d6e281f66db61f84091df1e1d97f57ed138b1bd5
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-09-14 11:08:01 -0300

    ✨ feat: Create ImagePresenter ToHTTPWithoutTag

--- src/infra/http/presenters/image.presenter.ts
+++ src/infra/http/presenters/image.presenter.ts
@@ -1,6 +1,18 @@
 import { Image } from '@/domain/alcremie/enterprise/entities/image';
 
 export class ImagePresenter {
+  static toHTTPWithoutTags(image: Image) {
+    return {
+      id: image.id.toValue(),
+      assetId: image.assetId,
+      isNsfw: image.isNsfw,
+      size: image.size,
+      url: image.url,
+      createdAt: image.createdAt,
+      updatedAt: image.updatedAt,
+    };
+  }
+
   static toHTTP(image: Image) {
     return {
       id: image.id.toValue(),
@@ -10,7 +22,7 @@ export class ImagePresenter {
       url: image.url,
       createdAt: image.createdAt,
       updatedAt: image.updatedAt,
-      tags: image.tags.map((tag) => ({ id: tag })),
+      tags: image.tags,
     };
   }
 }
