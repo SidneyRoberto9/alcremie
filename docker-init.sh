aa28682215933ca7974c7a302d7087b69be03a1e
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-10-09 23:55:01 -0300

    🔧 ci: remove docker compose

--- docker-init.sh
+++
@@ -1,4 +0,0 @@
-docker run --name mongodb -d -p 27017:27017 mongo mongod --replSet rs0
-docker exec -it mongodb mongosh 
-rs.initiate({_id: 'rs0', members: [{_id: 0, host: 'localhost:27017'}]})
-yarn prisma db push
